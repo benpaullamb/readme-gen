3502ff7252676a67620e945b48157f3a752d2b88
Ben <benpaullamb@gmail.com>
2021-09-26 18:47:03 +0100

    feat: added node and python options

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import click
 from pathlib import Path
 from string import Template
+import re
 
 @click.command()
 @click.option('--lang', default=None, help='Node or Python')
@@ -24,14 +25,40 @@ def cli(lang):
   template_data['title'] = readme.parts[-2]
   
   if lang is not None:
-    template_data = template_data | get_data_from_component(lang.lower())
+    if lang == 'node' or lang == 'python':
+      template_data = template_data | get_data_from_component(lang)
+    else:
+      print('Language not supported')
   
-  readme_template = Path('C:/Users/benpa/Documents/Projects/readme-generator/readme-template.md').read_text()
+  readme_template = Path(__file__).parent.joinpath('readme-template.md').read_text() 
   readme_output = Template(readme_template).safe_substitute(template_data)
   
   readme.write_text(readme_output)
 
-def get_data_from_component(component):
-  return {
-    'prereqs': '- Node'
+def get_data_from_component(component_name):
+  component = Path(__file__).parent.joinpath(f'./components/{component_name}.md')
+  
+  sections = {}
+
+  with component.open() as file:
+    current_section = ''
+    for line in file:
+      if line.startswith('#'):
+        current_section = re.search('#+\s*(.+)\n', line).group(1)
+        sections[current_section] = ''
+      else:
+        sections[current_section] += line
+
+  title_mapping = {
+    'Prerequisites': 'prereqs',
+    'Installation': 'install',
+    'Deployment': 'deploy'
   }
+
+  component_data = {}
+
+  for key, value in sections.items():
+    template_key = title_mapping[key]
+    component_data[template_key] = value.strip()
+
+  return component_data
---
+++ components/python.md
@@ -0,0 +1,27 @@
+### Prerequisites
+
+- Python
+
+### Installation
+
+1. Create a Python virtual environment.
+
+```
+python3 -m venv .venv
+```
+
+2. Activate the virtual environment.
+
+```
+// On Windows
+.venv/Scripts/Activate
+
+// On Mac
+source .venv/bin/activate
+```
+
+3. Install the CLI and dependencies.
+
+```
+python3 -m pip install -r requirements.txt
+```
